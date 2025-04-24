948766fa8c27ff4f01a5844c40629d3f288e9bf8
Raju Ahmmed <ahraju2003@gmail.com>
2025-04-24 23:01:34 +0600

    overdue borrow list get operation done

--- src/app/Modules/Borrow/borrow.route.ts
+++ src/app/Modules/Borrow/borrow.route.ts
@@ -4,16 +4,12 @@ import { BorrowController } from "./borrow.controller";
 const router = express.Router();
 
 // book borrow
-router.post("/", BorrowController.bookBorrow)
+router.post("/", BorrowController.bookBorrow);
 
 // return a book
-router.patch("/:borrowId", BorrowController.returnBook)
+router.patch("/:borrowId", BorrowController.returnBook);
 
 // overdue borrow list
-router.get("/overdue", BorrowController.overdueBorrowList)
-
-
-// // delete borrow
-// router.delete("/:borrowId", BorrowController.deleteBorrow)
+router.get("/overdue", BorrowController.overdueBorrowList);
 
 export const borrowRoutes = router;
